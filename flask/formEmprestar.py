b9fbb0b7e4f9451fa9c3bd4453d9bb2f0b5bb658
Moab Esdras <49200379+moabesdras@users.noreply.github.com>
2021-12-17 15:18:44 -0300

    Delete formEmprestar.py

--- flask/formEmprestar.py
+++
@@ -1,8 +0,0 @@
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class EmprestarForm(FlaskForm):
-    nome = StringField('Nome do Objeto', validators=[DataRequired()])
-    enviar = SubmitField('CADASTRAR')
