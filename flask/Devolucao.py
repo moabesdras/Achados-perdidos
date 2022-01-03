49b7a3cbcd6166a308ef78a1b699fc58917667a2
Moab Esdras <49200379+moabesdras@users.noreply.github.com>
2022-01-03 12:47:08 -0300

    Update Devolucao.py

--- flask/Devolucao.py
+++ flask/Devolucao.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import func
 
 class Emprestimo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    id_usuario = db.Column(db.Integer,db.ForeignKey('usuario.id'))
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
     nome_pessoa = db.Column(db.String(100), unique=False, nullable=True)
-    id_chave = db.Column(db.Integer,db.ForeignKey('chave.id'))
+    id_chave = db.Column(db.Integer, db.ForeignKey('chave.id'))
     data_emprestimo = db.Column(db.DateTime, unique=False, nullable=False, default=func.now())
